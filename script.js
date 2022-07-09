e1f808af95474bfa1955eaa67fa60f10ac187446
avship <alexandershipilo@yandex.ru>
2022-07-09 17:20:29 +0300

    Задание 15. Обрабатываем через keypress

--- script.js
+++ script.js
@@ -1,48 +1,84 @@
 document.addEventListener("DOMContentLoaded", function () {
-  class DomElement {
-    constructor(height = 100, width = 100, bg = "red", fontSize = null) {
-      this.height = height;
-      this.width = width;
-      this.bg = bg;
-      this.fontSize = fontSize;
-      this.figure = document.createElement("div");
-      this.figure.classList.add("test");
-      this.figure.classList.add(this.selector);
-      this.figure.style.position = "absolute";
-      this.figure.style.height = `${this.height}px`;
-      this.figure.style.width = `${this.width}px`;
-      this.figure.style.background = this.bg;
-      this.figure.style.top = "100px";
-      this.figure.style.left = "100px";
-      document.body.append(this.figure);
+  document.body.style.overflowX = "hidden";
+  document.body.style.overflowY = "hidden";
+
+  const DomElement = function (
+    selector = "#test",
+    height = 100,
+    width = 100,
+    bg = "red",
+    fontSize = "18"
+  ) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.item = null;
+    this.moveId = null;
+  };
+  DomElement.prototype.createElement = function () {
+    let text = "";
+    this.item = null;
+    if (this.selector.slice(0, 1) === ".") {
+      this.item = document.createElement("div");
+      this.item.classList.add(this.selector);
+      text = "div test";
+    } else if (this.selector.slice(0, 1) === "#") {
+      this.item = document.createElement("p");
+      this.item.setAttribute(
+        "id",
+        this.selector.slice(1, this.selector.length)
+      );
+      text = "p element";
     }
-    goUp() {
-      this.figure.style.top = `${parseInt(this.figure.style.top) - 10}px`;
+    this.item.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; position: absolute; top: 100px; left: 100px; line-height: 2.5; text-align: center;`;
+    this.item.textContent = text;
+    document.body.append(this.item);
+  };
+  DomElement.prototype.moveLeft = function () {
+    if (parseInt(this.item.style.left) - 10 <= 0) {
+      this.item.style.left = `${
+        document.body.clientWidth + parseInt(this.item.style.width)
+      }px`;
     }
-    goDown() {
-      this.figure.style.top = `${parseInt(this.figure.style.top) + 10}px`;
+    this.item.style.left = `${parseInt(this.item.style.left) - 10}px`;
+  };
+  DomElement.prototype.moveRight = function () {
+    if (parseInt(this.item.style.left) + 10 > document.body.clientWidth) {
+      this.item.style.left = `${-parseInt(this.item.style.width)}px`;
     }
-    goLeft() {
-      this.figure.style.left = `${parseInt(this.figure.style.left) - 10}px`;
+    this.item.style.left = `${parseInt(this.item.style.left) + 10}px`;
+  };
+  DomElement.prototype.moveUp = function () {
+    if (parseInt(this.item.style.top) + parseInt(this.item.style.height) < 0) {
+      this.item.style.top = `${
+        document.documentElement.clientHeight + parseInt(this.item.style.height)
+      }px`;
     }
-    goRight() {
-      this.figure.style.left = `${parseInt(this.figure.style.left) + 10}px`;
+    this.item.style.top = `${parseInt(this.item.style.top) - 10}px`;
+  };
+  DomElement.prototype.moveDown = function () {
+    if (parseInt(this.item.style.top) > document.documentElement.clientHeight) {
+      this.item.style.top = `${-parseInt(this.item.style.height)}px`;
     }
-  }
-  const square = new DomElement();
-  document.addEventListener("keyup", (event) => {
+    this.item.style.top = `${parseInt(this.item.style.top) + 10}px`;
+  };
+  const domEl = new DomElement();
+  domEl.createElement();
+  document.addEventListener("keydown", (event) => {
     switch (event.code) {
       case "ArrowUp":
-        square.goUp();
+        domEl.moveUp();
         break;
       case "ArrowDown":
-        square.goDown();
+        domEl.moveDown();
         break;
       case "ArrowLeft":
-        square.goLeft();
+        domEl.moveLeft();
         break;
       case "ArrowRight":
-        square.goRight();
+        domEl.moveRight();
         break;
     }
   });
